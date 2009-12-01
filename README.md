ba582aca4a42f34a729efc5aca0f14845f7087e5
Jed Schmidt <tr@nslator.jp>
2009-12-01 00:51:05 -0800

    added fab.js and readme

---
+++ README.md
@@ -0,0 +1,211 @@
+# (fab) / a DSL for building [node.js](http://nodejs.org/) apps
+
+## introduction
+
+(fab) makes it easy to build apps in node.js. just create a file:
+
+    // helloWorld.js
+    fab = require( "./fab" ).fab;
+
+    fab( "/", "hello world." ).deploy();
+    
+then run it in node:
+
+    $ node helloWorld.js
+    
+and then hit [http://localhost:4011/](http://localhost:4011/).
+
+## finding and binding
+
+just like you would use [jQuery](http://jquery.com) to *find* nodes in the DOM and *bind* event handlers to them, you can use (fab) to *find* parts of your site, and *bind* request handlers to them. so this in jQuery:
+
+    jQuery( node-to-find ).click( handler-to-bind );
+    
+would look like this in (fab):
+    
+    fab( path-to-find, request-handler-to-bind );
+
+## chaining
+    
+like jQuery, (fab) is a chained DSL. this means that instead of saving traversal targets locally, you can chain them together to keep things less verbose. the difference is that (fab) uses one overloaded function instead of a collection of methods, so instead of chaining methods, you're chaining functions.
+
+    ( fab )
+      ( "/time", function(){ return "the time is " + (new Date).toTimeString() } )
+      ( "/date", function(){ return "the date is " + (new Date).toDateString() } )
+    .deploy();
+
+its brevity may look unorthodox, but it's perfectly valid javascript, without the need for pre-compilation or `with` scoping magic.
+
+## nesting
+
+one goal of (fab) is to have the structure of your code reflect the structure of your site.
+
+    ( fab )
+      ( "/mysite/" )
+        ( function() { return "welcome to my site." } )
+
+        ( "pics/", function() { return "pictures coming soon." } )
+        ( "code/", function() { return "some of my code." } )
+      ()
+    .deploy();
+
+this is where the overloading comes in. instead of passing (fab) a path/handler pair, you can pass it:
+
+- a single path (like `"/mysite"` above), to create and return a child path,
+- a single handler (like the first function above), to bind the the current path, or
+- nothing, to return the parent path of the current context (like `.end()` in jQuery)
+
+## handling errors
+
+because you can define a site hierarchically, you can also have custom error handlers for each part of your site. so to modify the previous example:
+
+    ( fab )
+      ( "/mysite/" )
+        ( function() { return "welcome to my site." } )
+
+        ( "pics/" )
+          ( function() { return "pictures coming soon." } )
+          [ 404 ]( "picture not found" )
+        ()
+
+        ( "code/" )
+          ( function() { return "some of my code." } )
+          [ 404 ]( "code not found" )
+        ()
+      ()
+    .deploy();
+
+so `/mysite/badlink`, `/mysite/pics/badlink`, and `/mysite/code/badlink` can all return more helpful 404 errors.
+
+## getting and setting
+
+in addition to the original `request` and `response` arguments from node.js, each handler is called with a `this` object that makes it easy to get/set information about the current request/response. these functions are like `attr` and `css` from jQuery: one argument to get, and two to set.
+
+### this.location( property, [ value ] )
+
+specify only the property string to return information about the current request. you can use the same properties that you would use from `window.location`: `hash`, `host`, `hostname`, `href`, `pathname`, `port`, `protocol`, and `search`.
+
+    ( fab )
+      ( "/", function(){ return this.location( "host" ) })
+    .deploy();
+    
+or, specify a value to change any part of the current location. (fab) will interpret this as a redirect, and issue the appropriate `301` status and `Location` header:
+
+    ( fab )
+      ( "/", function(){ return this.location( "port", 4012 ) })
+    .deploy();
+
+### this.param( property, [ value ] )
+
+specify only a property to return the corresponding query string parameter, or specify a value to change the query string accordingly and issue a `301` redirect.
+
+### this.capture( position )
+
+in addition to strings, you can also use regular expressions to match paths, and return captures in order from the path:
+
+    ( fab )
+      ( /\/hello\/(\w+)/, function(){ return "hello, " + this.capture( 0 ) })
+    .deploy();
+
+eventually you'll also be able to set any capture and issue a `301` redirect, as with the `location` and `param` methods.
+
+### this.header( name, [ value ] )
+
+specify just a header name to get the corresponding request header, or specify a value to set the response header.
+
+### this.cookie( name, [ value, [ options ] ] )
+
+specify just a name to get a cookie value from the request, or add a value to set the cookie in the response. you can also send and options object with any or all of the following properties: `domain`, `path`, `expires`, and `secure`.
+
+### this.status( [ value ] )
+
+specify no arguments to return the current status, or a number to set the status and invoke the appropriate handler.
+
+## deploying
+
+once you've built your (fab) site, you can deploy on any part of it to deploy to a server.
+
+    fab = require( "./fab" ).fab;
+    http = require( "http" );
+    server = http.createServer();
+    server.listen( 4012 );
+    
+    ( fab )
+
+      ( "site1" )
+        ( "/", "this is running on port 4010" )
+      ().deploy( 4010 )
+
+      // 4011 is the default for (fab)
+      ( "site2" )
+        ( "/", "this is running on port 4011" )
+      ().deploy()
+      
+      // you can specify an existing server too
+      ( "site3" )
+        ( "/", "this is running on port 4012" )
+      ()
+      
+    .deploy( server );
+
+## extending (fab)
+
+(fab) is easy to extend with your own functions:
+
+    ( fab )
+      ( "/mysite/", "welcome to my site." )
+      ( "/mysite", addTrailingSlash )
+    .deploy();
+      
+    function addTrailingSlash() {
+      var path = this.location( "pathname" );
+      this.location( "pathname", path + "/" );
+    };
+
+and like jQuery, you can add your own methods to prototype of the fab object at `fab.fn` or to the prototype of the request/response object at `fab.env.fn`.
+
+## what's next
+
+though i'm not sure how much of this will actually come true due to time constraints, there are some things i think would be nice to add for future versions of (fab):
+
+### mulitple handlers for each bind, chained asynchronously
+
+right now you can only bind a single function to each part of a site, but eventually i'd like the ability to bind many, and chain them:
+
+    fab( /\/user\/(\w+)/, lookupUser, renderUser ).deploy();
+    
+    function lookupUser() { ... }
+    function renderUser() { ... }
+
+in this case, `lookupUser` might perform an asynchronous lookup from a remote datastore, and return a `process.Promise` object, for which `renderUser` would be its `addCallback` function. i haven't thought it through yet, but would love to have (fab) take advantage of javascript's asynchronous nature.
+
+### ability to deploy logic on the server or the browser
+
+i've tried to keep the node.js-specific logic separated so that it could be replaced with browser-specific logic, to allow (fab) to be deployed in a browser, using `window.location.hash` as the actual url. this could be convienient for deploying a certain part of the site that requires little dynamic data, such as a wizard interface, in a standalone browser-based app.
+
+## feedback
+
+feel free to [add an issue](http://github.com/jed/fab/issues) if you find a bug, or fork as you'd like and send me a pull request. you can also find me sometimes in #node.js on freenode, or on [twitter](http://twitter.com/jedschmidt).
+
+## license
+
+Copyright (c) 2009 Jed Schmidt ([http://jedschmidt.com](http://jedschmidt.com))
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ fab.js
@@ -0,0 +1,441 @@
+﻿;( function() {
+
+  var global = this,
+
+      http = require( "http" ),
+      sys = require( "sys" ),
+
+      statuses = {
+        100: "Continue",
+        101: "Switching Protocols",
+        200: "OK",
+        201: "Created",
+        202: "Accepted",
+        203: "Non-Authoritative Information",
+        204: "No Content",
+        205: "Reset Content",
+        206: "Partial Content",
+        300: "Multiple Choices",
+        301: "Moved Permanently",
+        302: "Moved Temporarily",
+        303: "See Other",
+        304: "Not Modified",
+        305: "Use Proxy",
+        400: "Bad Request",
+        401: "Unauthorized",
+        402: "Payment Required",
+        403: "Forbidden",
+        404: "Not Found",
+        405: "Method Not Allowed",
+        406: "Not Acceptable",
+        407: "Proxy Authentication Required",
+        408: "Request Time-out",
+        409: "Conflict",
+        410: "Gone",
+        411: "Length Required",
+        412: "Precondition Failed",
+        413: "Request Entity Too Large",
+        414: "Request-URI Too Large",
+        415: "Unsupported Media Type",
+        500: "Internal Server Error",
+        501: "Not Implemented",
+        502: "Bad Gateway",
+        503: "Service Unavailable",
+        504: "Gateway Time-out",
+        505: "HTTP Version Not Supported"
+      },
+
+      methods = [
+        "HEAD",
+        "GET",
+        "POST",
+        "PUT",
+        "DELETE",
+        "TRACE",
+        "OPTIONS",
+        "CONNECT"
+      ];
+      
+  exports.fab = fab;
+  
+  // for prototypal inheritance
+  function dummyFn(){};
+  function create( from ) {
+    dummyFn.prototype = from || this;
+    return new dummyFn;
+  };
+
+  // make iteration easier, c/o jQuery
+  function each( object, callback ) {
+    var name, i = 0, length = object.length;
+    
+    if ( length === undefined ) {
+      for ( name in object )
+        if ( callback.call( object[ name ], name, object[ name ] ) === false )
+          break;
+    } else
+      for ( var value = object[0];
+        i < length && callback.call( value, i, value ) !== false; value = object[++i] ){}
+
+    return object;
+  };
+    
+  // core fab object    
+  function fab( arg1, arg2 ) {
+
+    if ( this.constructor != fab )
+      return fab.apply( make(), arguments );
+      
+    if ( !arg1 )
+      return this.ls[ "../" ] || this;
+
+    if ( arg1.constructor === env )
+      return handle.call( this, arg1 );
+
+    if ( arg1.constructor === Function ) {
+      this.handler = arg1;
+      return this;
+    }
+      
+    if ( arg1.constructor === Number ) {
+      this.statuses[ arg1 ] = arg2;
+      return this;
+    }
+
+    if ( arg1.constructor === String )
+      arg1 = arg1.replace( /[.?]/g, "\\$&");
+  
+    if ( arg1.constructor === RegExp )
+      arg1 = arg1.source;
+
+    if ( arg1.constructor !== String )
+      throw "inavlid params";
+      
+    if ( arguments.length == 1 )
+      return append.call( this, arg1 );
+      
+    append.call( this, arg1 ).handler = arg2;
+    return this;
+  };
+
+  fab.fn = fab.prototype = {};  
+  fab.create = create;
+
+  // add default error handlers and corresponding methods
+  each( statuses, function( code, message ) {
+    statuses[ code ] = function() {
+      this._response.status = code;
+      this
+        .body( [
+          "<html>",
+            "<title>", code, ": ", message, "</title>",
+            "<body>", code, ": ", message, "</body>",
+          "</html>"
+        ].join("") )
+        .flush();
+    };
+
+    fab.fn[ code ] = function() {
+      var args = [ +code ];
+      args.push.apply( args, arguments );
+      this.apply( this, args );
+      return this;
+    };
+
+  });
+  
+  function handle( env ) {
+    var args = arguments,
+        path = env._request.location.pathname,
+        length = path.length,
+        lastFab = this,
+        thisFab, match, handler, ret;
+
+    each( this.ls, function( route ) {
+      // TODO: cache these regexps
+      match = path.match( RegExp( "^" + route ) );
+      if ( !match )
+        return true;
+
+      lastFab = this;
+      path = path.substr( match[ 0 ].length );
+      
+      Array.prototype.push.apply(
+        env._request.captures, match.slice( 1 )
+      );
+      
+      !path.length
+        ? thisFab = this
+        : each( this.ls, arguments.callee );
+              
+      return false;
+    });
+    
+    handler = thisFab
+      ? thisFab.handler || thisFab.statuses[ 501 ]
+      : lastFab.statuses[ 404 ];
+      
+    if ( typeof handler == "number" )
+      handler = thisFab.statuses[ handler ].call( env );
+
+    if ( typeof handler == "function" )
+      handler = handler.call( env );
+
+    if ( typeof handler == "string" )
+      env.body( handler );
+    
+    env.flush();
+  };
+  
+  function append( path ) {
+    var child = this.ls[ path ] = make();
+
+    child.ls[ "../" ] = this;
+    child.statuses = create( this.statuses );
+    
+    return child;
+  };
+
+  function make() {
+    var fn = fab.prototype,
+        ret = function() {
+          return fab.apply( arguments.callee, arguments );
+        };
+
+    ret.constructor = fab;
+    ret.ls = {};
+    ret.statuses = statuses;
+
+    for ( var name in fn )
+      ret[ name ] = fn[ name ];      
+
+    return ret;
+  };
+
+  // env to represent a request/response pair
+  fab.env = env;
+  function env() {
+    var self = create( env.fn );
+    self.constructor = env;
+    return env.fn.init.apply( self, arguments );  
+  };
+
+  env.fn = env.prototype = {
+
+    header: function( name, val ) {
+      if ( val === undefined )
+        return this._request.headers[ name ];
+        
+      this._response.headers[ name ] = val;
+      return this;
+    },
+
+    status: function( val ) {
+      if ( val === undefined )
+        return this._response.status;
+        
+      this._response.status = +val;
+      this.fab.statuses[ val ].apply( this, this._arguments );
+      
+      return this;    
+    },
+    
+    location: function( name, val ) {
+      if ( val === undefined )
+        return this._request.location[ name ];
+          
+      this._response.location[ name ] = val;
+      this._redirect = true;
+      return this;    
+    },
+
+    cookie: function( name, val, options ) {
+      if ( val === undefined )
+        return this._request.cookies[ name ];
+      
+      options = options || {};
+      options.value = val;
+      options.path = options.path || "/";
+
+      this._response.cookies[ name ] = options;
+      return this;    
+    },
+
+    param: function( name, val ) {
+      if ( val === undefined )
+        return this._request.params[ name ];
+    
+      this._response.params[ name ] = val;
+      this._redirect = true;      
+      return this;
+    },
+
+    capture: function( pos, val ) {
+      if ( val === undefined )
+        return this._request.captures[ +pos ];
+        
+      this._response.captures[ +pos ] = val;
+      this._redirect = true;      
+      return this;
+    },
+
+    body: function( val ) {
+      this._response.body = val.toString();
+      return this;
+    }
+  }
+
+  // init "constructor", specific to node.js
+  fab.env.fn.init = function( req, res ) {
+    var hostPort = req.headers.host.split( ":" ),
+        cookieHeader = req.headers["cookie"],
+        cookies = {};
+        
+    if ( cookieHeader )
+      cookieHeader.split(";").forEach( function( cookie ) {
+        var parts = cookie.split("="),
+            name = parts[0].trim(),
+            value = parts[1].trim();
+            
+        cookies[ name ] = value;
+      });
+        
+    this._request = {
+      method: req.method,
+  
+      headers: req.headers,
+      cookies: cookies,
+  
+      location: {
+        hash: req.uri.fragment,
+        host: hostPort[0],
+        href: "http://" + req.headers.host + req.uri.full,
+        hostname: req.headers.host,
+        pathname: req.uri.path,
+        port: hostPort[1],
+        protocol: "http:",
+        search: req.uri.queryString
+      },
+      captures: { length: 0 },
+      params: req.uri.params,
+  
+      original: req
+    };
+    
+    this._response = {
+      status: 200,
+  
+      headers: { "content-type": "text/html; charset=UTF-8" },
+      cookies: {},
+  
+      location: fab.create( this._request.location ),
+      captures: fab.create( this._request.captures ),
+      params: fab.create( this._request.params ),
+  
+      body: "",
+  
+      original: res
+    };
+    
+    this._redirect = false;
+    
+    return this;
+  };
+  
+  // flush method, specific to node.js
+  fab.env.fn.flush = function() {
+    var res = this._response,
+        original = res.original,
+        cookies = res.cookies;
+  
+    res.headers[ "content-length" ] = process._byteLength( res.body );
+    renderCookies();
+    
+    if ( this._redirect ) {
+      res.status = 302;
+      res.headers[ "location" ] = buildUrl();
+    }
+    
+    original.sendHeader( res.status, res.headers );
+    original.sendBody( res.body, "utf-8" );
+    original.finish();
+    
+    function buildUrl() {
+      var loc = res.location,
+          url = [ loc.protocol, "//" ],
+          name, queryString = [];
+
+      if ( loc.hasOwnProperty( "href" ) )
+        return loc.href;
+          
+      if ( loc.hasOwnProperty( "hostname" ) )
+        url.push( loc.hostname )
+
+      else {
+        url.push( loc.host );
+        if ( loc.port )
+          url.push( ":", loc.port );
+      }
+      
+      // TODO: figure out a way to replace regex captures
+      url.push( loc.pathname );
+      
+      if ( loc.hasOwnProperty( "search" ) )
+        queryString.push( "?", loc.search )
+
+      else
+        for ( name in res.params )
+          queryString.push(
+            queryString.length ? "&" : "?",
+            name, "=", res.params[ name ]
+          );
+  
+      url.push.apply( url, queryString );
+
+      if ( loc.hash )
+        url.push( "#", loc.hash );
+        
+      return url.join("");    
+    }
+    
+    function renderCookies() {
+      var ret, name, options;
+  
+      for ( name in cookies ) {
+        ret = [];
+          
+        ret.push( name, "=", cookies[ name ].value )
+        
+        options = cookies[ name ];
+      
+        if ( options.expires )
+          ret.push( "; expires=", options.expires.toUTCString() );
+            
+        if ( options.path )
+          ret.push( "; path=", options.path );
+        
+        if ( options.domain )
+          ret.push( "; domain=", options.domain );
+      
+        if ( options.secure )
+          ret.push( "; secure" );
+          
+        res.headers[ "set-cookie-" + name ] = [ "set-cookie", ret.join("") ];
+      }
+    }
+  };
+  
+  // deploy method, specific to node.js
+  fab.fn.deploy = function() {
+    var self = this;
+  
+    http.createServer( function( req, res ) {
+  
+      self( fab.env( req, res ) );
+  
+    }).listen( 0xFAB )
+    
+    return this;
+  };  
+
+})();
