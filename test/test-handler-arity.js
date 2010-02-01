cf93e0f137c8c542feb54406e27e64be16de6626
Jed Schmidt <tr@nslator.jp>
2010-02-01 00:03:06 -0500

    added handler arity test.

---
+++ test/test-handler-arity.js
@@ -0,0 +1,38 @@
+var
+  PORT = 0xFAB,
+
+  fab = require( "../" ),
+  http = require( "http" ),
+  assert = require( "assert" ),
+  
+  client = http.createClient( PORT ),
+  server = http.createServer( 
+  
+    ( fab )
+      ( "/onearg", function onearg( respond ) {
+        assert.equal( onearg.length, 1 );
+        assert.notEqual( this, respond );
+        
+        respond( this.url.pathname, null );
+      } )
+      ( "/twoargs", function twoarg( request, respond ) {
+        assert.equal( twoarg.length, 2 );
+        assert.equal( this, request );
+
+        respond( this.url.pathname, null );
+      } )
+    ( fab )
+  
+  );
+  
+server.listen( PORT );
+
+client
+  .request( "/onearg" )
+  .finish( function( response ) {
+    response.addListener( "complete", function(){
+      client
+        .request( "/twoargs" )
+        .finish( function() { server.close() })
+    })
+});
