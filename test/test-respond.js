8f6581d4ffde5e85a49fc928c382914521eaab28
Felix Geisendörfer <felix@debuggable.com>
2010-02-08 15:25:03 +0100

    Test case for respond bug
    repond(obj, null) does seems to be broken.

---
+++ test/test-respond.js
@@ -0,0 +1,56 @@
+require('./common');
+
+var
+  PORT = 0xFAB,
+
+  expectedCallbacks = {
+    a1: 1,
+    a2: 1,
+    b1: 1,
+    b2: 1,
+  };
+
+  client = http.createClient(PORT),
+  server = http.createServer(
+    (fab)
+      ('/a', function(respond) {
+        respond({
+          body: 'test'
+        });
+        respond(null);
+        expectedCallbacks.a1--;
+      })
+      ('/b', function(respond) {
+        respond({
+          body: 'test'
+        }, null);
+        expectedCallbacks.b1--;
+      })
+    (fab)
+  ),
+
+  timeout = setTimeout(function() {
+    checkCallbacks(expectedCallbacks);
+  }, 1000);
+  
+server.listen(PORT);
+
+client
+  .request('/a')
+  .finish(function(response) {
+    puts('a');
+    expectedCallbacks.a2--;
+  });
+
+client
+  .request('/b')
+  .finish(function(response) {
+    puts('b');
+    expectedCallbacks.b2--;
+    clearTimeout(timeout);
+    server.close();
+  });
+
+process.addListener('exit', function() {
+  checkCallbacks(expectedCallbacks);
+});
