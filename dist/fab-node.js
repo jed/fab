21fc949f89404631a81b2541e7b2446552550c60
Jed Schmidt <tr@nslator.jp>
2010-03-16 12:54:27 +0900

    removed host from head.url, added remote app.

--- dist/fab-node.js
+++ dist/fab-node.js
@@ -322,11 +322,7 @@ fab.listener = function() {
       down = down({
         method: request.method,
         headers: request.headers,
-        url: url.parse(
-          "http://" +
-          request.headers.host +
-          request.url
-        )
+        url: url.parse( request.url )
       });
     }
     
@@ -374,6 +370,41 @@ fab.listener = function() {
         
         response.close();
       }
+    }
+  }
+}
+
+fab.remote = function() {
+  var
+    url = require( "url" ),
+    http = require( "http" ),
+    loc = url.parse( arguments[ 0 ] ),
+    client = http.createClient( loc.port || 80, loc.hostname );
+
+  return function( back ) {
+    return function( head ) {
+      head.headers.host = loc.hostname;
+      
+      client
+        .request(
+          head.method,
+          loc.pathname + head.url.pathname + ( head.url.search || "" ),
+          head.headers
+        )
+        .addListener( "response", function( response ) {
+          back({
+            status: response.statusCode,
+            headers: response.headers
+          });
+        
+          response
+            .addListener( "data", function( chunk ) {
+              back({ body: chunk });
+            })
+            .addListener( "end", back )
+            .setBodyEncoding( "utf8" );
+        })
+        .close();
     }
   }
 }
