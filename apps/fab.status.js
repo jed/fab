b5b2c8d976ef4f4b84acb509ed58bdbc026e0291
Jed Schmidt <tr@nslator.jp>
2010-04-23 11:54:07 -0400

    added 3 tests for fab.status.

--- apps/fab.status.js
+++ apps/fab.status.js
@@ -1,9 +1,40 @@
 exports.summary = "Turns number into an app that responds with it as the status code.";
 
-exports.app = function( code ) {
+function status( code ) {
   code = +code;
   return function() {
     var out = this({ status: code });
     if ( out ) out();
   }
 }
+
+exports.tests = ( function() {
+  var response = 200
+    , app = status( response )
+    , fn = function(){};
+
+  return [
+
+    function
+    statusReturnsUnaryApp() {
+      this( app.length === 0 )
+    },
+    
+    function
+    statusRespondsWithCorrectPayload() {
+      var out = this;
+      app.call( function( obj ){ out( obj.status === response ) } );
+    },
+  
+    function
+    statusClosesConnection() {
+      var out = this;
+      app.call( function() {
+        return function(){ out( !arguments.length ) }
+      })
+    }
+  ];
+  
+})();
+
+exports.app = status;
