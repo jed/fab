1ea3703a6fd03e209479b15936297e9e5e849be2
Jed Schmidt <tr@nslator.jp>
2010-04-25 16:30:22 -0400

    changed fab.Number to use fab.status only for valid HTTP codes.

--- apps/fab.Number.js
+++ apps/fab.Number.js
@@ -1,5 +1,11 @@
-var fab = { status: require( "./fab.status" ).app };
+var fab = {
+  status: require( "./fab.status" ).app,
+  body: require( "./fab.body" ).app
+}
 
 exports.app = function( code ) {
-  return fab.status.apply( this, arguments );
+  var isHttpStatus = code < 506 &&
+    code % 100 <= [ 0, 1, 6, 7, 17, 5 ][ code.toString()[0] ];
+
+  return fab[ isHttpStatus ? "status" : "body" ]( code );
 }
