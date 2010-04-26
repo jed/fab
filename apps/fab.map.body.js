bfb83fb60375e5164b417c5fb5fe0e0dac58d7db
Jed Schmidt <tr@nslator.jp>
2010-04-26 09:27:39 -0400

    added map app for bodies only.

---
+++ apps/fab.map.body.js
@@ -0,0 +1,16 @@
+exports.summary = "Turns a function into a binary app that maps response bodies from the upstream app.";
+
+exports.app = function( fn ) {
+  return function( app ) {
+    return function() {
+      var out = this;
+      return app.call( function listener( obj ) {
+        var body = obj && obj.body;
+        if ( body ) arguments[ 0 ].body = fn.call( body, body );
+        out = out.apply( this, arguments );
+        
+        return listener;
+      })
+    }
+  }
+}
